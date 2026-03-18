a3897c403c84ae25bd80755e1f5ba1f7687c0766
CodeGauransh <kumar.gauransh@gmail.com>
2026-03-18 10:27:40 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataValidationConfig
 import sys
 
 
@@ -15,7 +16,13 @@ if __name__=='__main__':
         DataIngestion = DataIngestion(dataingestionconfig)
         logging.info("Starting the data ingestion process")
         dataingestionartifact = DataIngestion.initiate_data_ingestion()
-
+        logging.info("Data ingestion process completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Starting the data validation process")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation process completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -12,7 +12,7 @@ FILE_NAME:str = "phishingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
 
-
+SCHEMA_FILE_PATH:str = os.path.join("data_schema","schema.yaml")
 
 
 DATA_INGESTION_COLLECTION_NAME:str =  "PhishingData"
@@ -21,3 +21,15 @@ DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float = 0.2
+
+
+
+"""Starting with Data Validation Constants"""
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_VALIDATED_DIR:str = "validated"
+DATA_VALIDATION_INVALID_DIR:str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
+
+
